138055829bf7e66b4ab6b1278a7d4c0e843662a7
Rakholiya-Vivek <rakholiyavivek2004@gmail.com>
2025-09-24 19:50:39 +0530

    updated

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,6 +1,6 @@
-output "ecr_repository_url" {
-  value = aws_ecr_repository.strapi.repository_url
-}
+# output "ecr_repository_url" {
+#   value = aws_ecr_repository.strapi.repository_url
+# }
 
 output "instance_public_ip" {
   value = aws_instance.strapi.public_ip
