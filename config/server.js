5430af661bde1633d967aabdbef9d76c7a8ee4c0
Rakholiya-Vivek <rakholiyavivek2004@gmail.com>
2025-09-26 11:20:55 +0530

    server.js updated

--- config/server.js
+++ config/server.js
@@ -1,3 +1,18 @@
+// module.exports = ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+//   webhooks: {
+//     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+//   },
+
+//   allowedHosts: [
+//     'my-strapi-project-vivek-alb-1419655971.ap-south-1.elb.amazonaws.com',
+//   ],
+// });
+
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
@@ -7,8 +22,15 @@ module.exports = ({ env }) => ({
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
-
+  settings: {
+    cors: {
+      enabled: true,
+      origin: ['*'], // or restrict to specific domains
+    },
+  },
   allowedHosts: [
-    'my-strapi-project-vivek-alb-1419655971.ap-south-1.elb.amazonaws.com',
+    'localhost',
+    '127.0.0.1',
+    'my-strapi-project-vivek-alb-1419655971.ap-south-1.elb.amazonaws.com'
   ],
 });
